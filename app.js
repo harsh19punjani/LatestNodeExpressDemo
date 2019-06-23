29714afc8c3b17c3661054c51a6a8569acb1934d
PUVI <puvi@senncom.com>
2019-06-23 17:11:00 +0530

    logger winston and morgan helmet compression

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 //express templating engine
 var hbs = require('express-handlebars');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+//helmet avoid Cross-site scripting (XSS) attack (security purpose)
+var helmet = require('helmet');
+var compression = require('compression');
+var rfs = require('rotating-file-stream');
+var fs = require('fs');
+//winston logger
+var loggerutil = require('./utilities/logger');
+var datalogger = require('./utilities/datalogger');   
 
 /* append routes file in app*/
 var router=require('./router');
@@ -50,19 +55,58 @@ app.use(require('express-status-monitor')({
   }]
 }));
 
+// compress all responses
+app.use(compression());
+
 // view engine setup - Express-Handlebars
 app.engine('hbs', hbs({
   extname: 'hbs',
   defaultLayout: 'layout',
   layoutsDir: __dirname + '/views/'
 }));
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// Linking log folder and ensure directory exists
+var logDirectory = path.join(__dirname, 'log');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+fs.appendFile('./log/ServerData.log', '', function (err) {
+  if (err) throw err;
+});
+
+// Create a rotating write stream
+var accessLogStream = rfs('Server.log', {
+  interval: '1d', // rotate daily
+  path: logDirectory
+});
+
+// uncomment to redirect global console object to log file
+// datalogger.logfile();
+
+// Helmet helps for securing Express apps by setting various HTTP headers
+app.use(helmet());
+
+// Generating date and time for logger
+logger.token('datetime', function displayTime() {
+  return new Date().toString();
+});
+
+// Allowing access headers and requests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "HEAD, OPTIONS, GET, POST, PUT, PATCH, DELETE, CONNECT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+// defining mode of logging
 app.use(logger('dev'));
+app.use(logger(':remote-addr :remote-user :datetime :req[header] :method :url HTTP/:http-version :status :res[content-length] :res[header] :response-time[digits] :referrer :user-agent', {
+    stream: accessLogStream
+}));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -85,5 +129,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+loggerutil.debug('debug message');
+loggerutil.error('error message');
+loggerutil.silly('silly message');
+loggerutil.verbose('verbose message');
+loggerutil.warn('warn message');
+loggerutil.info('info message');
 
 module.exports = app;
